ddfe67d2fd5ae014d062c3cde09fece3b642150b
antony <antony.joseph@bdb.ai>
2024-07-10 13:22:33 +0530

    added server

---
+++ server/src/models/user.ts
@@ -0,0 +1,10 @@
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password?: string;
+    mobile: string;
+    description: string;
+    dob: Date;
+    img: string;
+}
---
+++ server/src/services/userService.ts
@@ -0,0 +1,26 @@
+import User from '../entities/userEntity';
+import { IUser } from '../models/user';
+
+class UserService {
+    async getAllUsers(): Promise<IUser[]> {
+        return User.find();
+    }
+
+    async getUserById(userId: string): Promise<IUser | null> {
+        return User.findById(userId);
+    }
+
+    async createUser(user: IUser): Promise<IUser> {
+        return User.create(user);
+    }
+
+    async updateUser(userId: string, updatedUser: IUser): Promise<IUser | null> {
+        return User.findByIdAndUpdate(userId, updatedUser, { new: true });
+    }
+
+    async deleteUser(userId: string): Promise<void> {
+        await User.findByIdAndDelete(userId);
+    }
+}
+
+export default new UserService();
